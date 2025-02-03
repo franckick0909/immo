e84e4e6dc3efe57a616cf1b3ad1347e8b6145d23
Franck Chapelon <franckick2@gmail.com>
2025-02-03 09:09:10 +0100

    commit 4

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
 export { auth as middleware } from "@/lib/auth"
+
+import { NextResponse } from "next/server"
+import { auth } from "@/lib/auth"
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth
+  const isOnDashboard = req.nextUrl.pathname.startsWith("/dashboard")
+
+  if (isOnDashboard && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", req.url))
+  }
+
+  return NextResponse.next()
+})
+
+// Spécifiez les chemins qui doivent déclencher ce middleware
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,30 @@
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async jwt({ token, user }) {
+      // Ajoute les données de l'utilisateur au token lors de la première connexion
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.name
+        token.picture = user.image
+      }
+      return token
+    },
+    async session({ session, token }) {
+      // Ajoute les informations du token à la session
+      if (session.user) {
+        session.user.id = token.id as string
+        session.user.email = token.email as string
+        session.user.name = token.name as string
+        session.user.image = token.picture as string
+      }
+
+      return session
+    }
+  },
   session: { strategy: "jwt" },
   ...authConfig,
-});
+})
