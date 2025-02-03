e5443760fb21c4d94290a5b61e139e7b00886d6a
Franck Chapelon <franckick2@gmail.com>
2025-02-03 13:15:21 +0100

    commit 5

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,13 @@
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   callbacks: {
     async jwt({ token, user }) {
       // Ajoute les données de l'utilisateur au token lors de la première connexion
